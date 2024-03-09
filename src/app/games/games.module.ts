5a6bbcb0917df756951cf012813a05d8a493efc9
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-09 17:05:18 +0200

    added components to games module

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AllGamesComponent } from './all-games/all-games.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
+import { EditGameComponent } from './edit-game/edit-game.component';
+import { AddGameComponent } from './add-game/add-game.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AllGamesComponent,
+    GameDetailsComponent,
+    EditGameComponent,
+    AddGameComponent
+  ],
   imports: [
     CommonModule
   ]
