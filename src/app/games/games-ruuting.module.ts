c4bca3cbf7c1a4319c070da23b0754b64b58b1e7
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-14 08:05:45 +0200

    adding paths

--- src/app/games/games-ruuting.module.ts
+++ src/app/games/games-ruuting.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllGamesComponent } from './all-games/all-games.component';
+import { AddGameComponent } from './add-game/add-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'games', children: [
+      { path: '', pathMatch: 'full', component: AllGamesComponent },
+      { path: 'add', component: AddGameComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
