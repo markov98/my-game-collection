91f90c2974d637e1516c44834ac10544e4beb43c
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-20 16:06:51 +0200

    added directives to login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor() { };
 
+  login(form: NgForm) {
+    if (form.invalid) {
+      alert("Error!")
+    } else {
+      alert('Seccess!')
+    }
+  }
 }
