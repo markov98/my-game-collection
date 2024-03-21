baacc441ae1fa22feb747c3a5c14dd8de1ca9f5d
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-21 16:35:29 +0200

    update register component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor() { };
 
+  register(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+
+    alert('Seccess!')
+  }
 }
