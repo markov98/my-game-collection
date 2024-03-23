c21a5314187b47517f82d207972e15eeb0521b74
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-23 16:39:29 +0200

    working on AddGameCoponent

--- src/app/games/add-game/add-game.component.ts
+++ src/app/games/add-game/add-game.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-game',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-game.component.css']
 })
 export class AddGameComponent {
+  form: FormGroup = this.formBuilder.group({
+    name: ['', Validators.required],
+    year: ['', Validators.required],
+    developer: ['', Validators.required],
+  });;
 
+  constructor(private formBuilder: FormBuilder) { }
 }
