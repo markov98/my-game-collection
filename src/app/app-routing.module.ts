3041b370dff95fd93a0e8bc10a080f394a82e0a6
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-28 15:55:38 +0200

    added home to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', pathMatch: 'full', component: HomeComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
