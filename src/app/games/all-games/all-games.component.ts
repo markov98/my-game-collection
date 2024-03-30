03ac7c706c1569fb4ce306d9900ebb4a51c2c301
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-30 17:25:13 +0200

    all games now works

--- src/app/games/all-games/all-games.component.ts
+++ src/app/games/all-games/all-games.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Game } from 'src/app/types/game';
 
 @Component({
   selector: 'app-all-games',
   templateUrl: './all-games.component.html',
   styleUrls: ['./all-games.component.css']
 })
-export class AllGamesComponent {
+export class AllGamesComponent implements OnInit {
+  games : [string, Game][] = []
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getGames().subscribe(result => {
+      this.games = Object.entries(result);
+      console.log(this.games);
+      
+    } );
+  }
 }
--- src/app/games/game-card/game-card.component.ts
+++ src/app/games/game-card/game-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Game } from 'src/app/types/game';
 
 @Component({
   selector: 'app-game-card',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-card.component.css']
 })
 export class GameCardComponent {
+  @Input() game!: [string, Game];
+  gameInfo: Game = {} as Game;
+  gameId: string = '';
 
+  // Initialize the gameInfo and gameId properties when the input changes
+  ngOnChanges(): void {
+    if (this.game) {
+      this.gameInfo = this.game[1];
+      this.gameId = this.game[0];
+    }
+  }
 }
